68adc3a062ef94bea91ed1d4ab4aad45076335bf
bloodgroup-cplusplus <81256027+bloodgroup-cplusplus@users.noreply.github.com>
2025-03-06 13:58:59 +0530

    adding new services

--- main.go
+++ main.go
@@ -1,21 +1,15 @@
 package main
 
 import (
-	"fmt"
-	"html"
 	"log"
-	"net/http"
+
+	"github.com/bloodgroup-cplusplus/go_docker/cmd/api"
 )
 
-func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
+func main () {
+	server := api.NewAPIServer(":8080",nil)
 
-	})
-
-	http.HandleFunc("/hi", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hi")
-	})
-
-	log.Fatal(http.ListenAndServe(":8081", nil))
+	if err := server.Run(); err !=nil {
+		log.Fatalf(err)
+	}
 }
